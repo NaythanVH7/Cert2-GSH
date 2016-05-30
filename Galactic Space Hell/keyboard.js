f8b031d862a5c5c838660c59e14e460521d4c65c
NaythanVH7 <n.vh7@hotmail.com>
2016-05-30 19:29:36 +1000

    main,player,keyboard
    added the keyboard and player js files. add updates to the main.js file

---
+++ Galactic Space Hell/keyboard.js
@@ -0,0 +1,84 @@
+var Keyboard = function()
+{
+	var self = this;
+
+	window.addEventListener('keydown', function(evt) {self.onKeyDown(evt); }, false);
+	window.addEventListener('keyup', function(evt) {self.onKeyUp(evt); }, false);
+
+	this.keyListeners = new Array();
+	this.keys = new Array();
+
+	this.KEY_SPACE = 32;
+	this.KEY_LEFT = 37;
+	this.KEY_UP = 38;
+	this.KEY_RIGHT = 39;
+	this.KEY_DOWN = 40;
+
+	this.KEY_A = 65;
+	this.KEY_D = 68;
+	this.KEY_S = 83;
+	this.KEY_W = 87;
+	this.KEY_SHIFT = 16;
+	this.KEY_ENTER = 13;
+
+	Keyboard.prototype.onKeyDown = function(evt)
+	{
+		this.keys[evt.keyCode] = true;
+
+		if(event.keyCode == KEY_UP)
+		{
+			player.directionY = 3;
+		}
+
+		if(event.keyCode == KEY_DOWN)
+		{
+			player.directionY = -3;
+		}
+
+		if(event.keyCode == KEY_LEFT)
+		{
+			player.directionX = -3;
+		}
+
+		if(event.keyCode == KEY_RIGHT)
+		{
+			player.directionX = 3;
+		}
+
+		if(event.keyCode == KEY_SPACE && shootTimer <= 0)
+		{
+			shootTimer += 0.3;
+			playerShoot();
+		}
+	};
+
+	Keyboard.prototype.onKeyUp = function(evt)
+	{
+		this.keys[evt.keyCode] = false;
+
+		if(event.keyCode == KEY_UP)
+		{
+			player.directionY = 0;
+		}
+
+		if(event.keyCode == KEY_DOWN)
+		{
+			player.directionY = 0;
+		}
+
+		if(event.keycode == KEY_LEFT)
+		{
+			player.directionX = 0;
+		}
+
+		if(event.keyCode == KEY_RIGHT)
+		{
+			player.directionX = 0;
+		}
+	};
+
+	Keyboard.prototype.isKeyDown = function(keyCode)
+	{
+		return this.keys[keyCode];
+	};
+}
